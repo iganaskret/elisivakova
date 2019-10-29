340916a9d161ad48a498f806d70b428d8a6ec25e
Iga <iga.naskret@gmail.com>
2019-10-29 09:34:08 +0100

    responsivity

---
+++ elisivakova-master/js/js.js
@@ -0,0 +1,15 @@
+function cv() {
+    window.open ("http://eliskasivakova.dk/CV_Eliska_Sivakova.pdf");
+}
+
+function btn1() {
+    window.open ("https://www.instagram.com/elisivakova/");
+}
+
+function btn2() {
+    window.open ("https://www.linkedin.com/in/eli%C5%A1ka-siv%C3%A1kov%C3%A1-920843134/");
+}
+
+function btn3() {
+    window.open ("https://www.behance.net/elisivakovd6b3");
+}
